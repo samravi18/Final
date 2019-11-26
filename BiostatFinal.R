1e34ca02d332e06e1a24d7f933532bc46f5b62ca
Samhita Ravi <sar160@pitt.edu>
2019-11-26 13:57:06 -0500

    Made boxplot to describe the data.

--- BiostatFinal.R
+++ BiostatFinal.R
@@ -3,3 +3,11 @@ Yes
 library("readxl")
 beer <- read_excel(file.choose())
 head(beer)
+
+
+beer$glassShape <- factor(beer$glassShape, levels = c("straight", "curved"))
+
+pdf("Beerboxplot.pdf")
+boxplot(drinkingMinutes ~ glassShape, data = beer)
+dev.off()
+
