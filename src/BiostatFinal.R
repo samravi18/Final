c467736cec42ae48c9d271528add887bae2b8991
Samhita Ravi <sar160@pitt.edu>
2019-11-27 13:53:38 -0500

    New file replace

--- src/BiostatFinal.R
+++ src/BiostatFinal.R
@@ -11,7 +11,8 @@ pdf("Beerboxplot.pdf")
 boxplot(drinkingMinutes ~ glassShape, data = beer)
 dev.off()
 
+
 t.test(beer$drinkingMinutes ~ beer$glassShape, data = beer, var.equal = TRUE)
-
-
-
+#Code used to save output. It also saves the history of other outputs.
+sink("newconsole.txt")
+savehistory("newconsole.txt")
