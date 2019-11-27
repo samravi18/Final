ff3236379f328f71e3610d572ea7aec3de6bd18e
Samhita Ravi <sar160@pitt.edu>
2019-11-27 13:04:02 -0500

    Add Code

---
+++ src/BiostatFinal.R
@@ -0,0 +1,17 @@
+install.packages("readxl")
+Yes
+library("readxl")
+beer <- read_excel(file.choose())
+head(beer)
+
+
+beer$glassShape <- factor(beer$glassShape, levels = c("straight", "curved"))
+
+pdf("Beerboxplot.pdf")
+boxplot(drinkingMinutes ~ glassShape, data = beer)
+dev.off()
+
+t.test(beer$drinkingMinutes ~ beer$glassShape, data = beer, var.equal = TRUE)
+
+
+
