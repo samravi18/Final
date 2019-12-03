e48e8138c5eb560ea1f6bdac1df8401c2cbab003
Samhita Ravi <sar160@pitt.edu>
2019-12-02 20:17:44 -0500

    changed files because deleted a value from straight data.

--- src/BiostatFinal.R
+++ src/BiostatFinal.R
@@ -1,4 +1,4 @@
-beer <- read.csv(url("https://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter12/chap12q32BeerGlassShape.csv"))
+beer <- read.csv(file.choose())
 head(beer)
 
 
@@ -9,12 +9,14 @@ boxplot(drinkingMinutes ~ glassShape, data = beer)
 dev.off()
 
 yourOutput<-t.test(beer$drinkingMinutes ~ beer$glassShape, data = beer, var.equal = TRUE)
+yourOutput
 #Saves results in a textfile
 cat("Results", file = "tests.txt", append = TRUE)
 capture.output(yourOutput, file = "tests.txt", append = TRUE)
 
 #one push button for all code: the boxplot will appeared as a saved file. The textfile titled tests.txt will have the saved t-test output.
 source("BiostatFinal.R")
+
 
 
 
